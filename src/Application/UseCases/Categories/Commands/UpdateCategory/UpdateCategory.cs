4fadf78238a82971fa8841e2dd47224cee72dc5e
Le Minh Pham <naminhvd@gmail.com>
2024-05-20 13:34:56 +0700

    Added a migration to update Categories table, renamed old migrations, and added placeholder for new services

---
+++ src/Application/UseCases/Categories/Commands/UpdateCategory/UpdateCategory.cs
@@ -0,0 +1,32 @@
+﻿using Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Categories.Commands.UpdateCategory
+{
+    public class UpdateCategoryCommand : IRequest
+    {
+        public required int Id { get; init; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+
+    }
+
+    public class UpdateCategoryCommandHandler : IRequestHandler<UpdateCategoryCommand>
+    {
+        private readonly IApplicationDbContext dbContext;
+
+        public UpdateCategoryCommandHandler(IApplicationDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+
+        public async Task Handle(UpdateCategoryCommand request, CancellationToken cancellationToken)
+        {
+
+        }
+    }
+}
--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Category entity
     /// </summary>
-    public class Category : BaseEntity<int>
+    public class Category : BaseAuditableEntity<int>
     {
         public required string Name {  get; set; }
         public required string Description { get; set; }
