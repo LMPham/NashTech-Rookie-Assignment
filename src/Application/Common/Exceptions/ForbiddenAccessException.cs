e5aa2bd7e4c5c48ea3798c11e1cb28a7ec907651
Le Minh Pham <naminhvd@gmail.com>
2024-05-27 11:52:39 +0700

    Added exception handlers

--- src/Application/Common/Exceptions/ForbiddenAccessException.cs
+++ src/Application/Common/Exceptions/ForbiddenAccessException.cs
@@ -1,5 +1,9 @@
 ﻿namespace Application.Common.Exceptions
 {
+    /// <summary>
+    /// Represents an error that occurs when the user is
+    /// unauthorized to access a resource.
+    /// </summary>
     public class ForbiddenAccessException : Exception
     {
         public ForbiddenAccessException() : base() { }
